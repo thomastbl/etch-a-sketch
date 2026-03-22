e24d9ea1dea410f41611439e6405ab51e620d074
thomastbl <101262386+thomastbl@users.noreply.github.com>
2026-03-22 03:48:08 +0100

    feature: implement slider to actively change grid size

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 const gridSizeInput = document.querySelector(".grid-input");
+const flexContainer = document.querySelector(".flex-container");
 gridSizeInput.addEventListener("input", () => {
+  flexContainer.style.width = gridSizeInput.value + "px";
+  flexContainer.style.height = gridSizeInput.value + "px";
   console.log(gridSizeInput.value);
 });
