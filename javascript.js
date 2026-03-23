f4348e6a99749308d1a68ee3fd225b41d80c1b7d
thomastbl <101262386+thomastbl@users.noreply.github.com>
2026-03-23 17:25:00 +0100

    feat: implement drawing functionality

--- javascript.js
+++ javascript.js
@@ -1,24 +1,45 @@
 function fillGrid() {
-  flexContainer.innerHTML = "";
-  flexContainer.style.width = gridSizeInput.value + "px";
-  flexContainer.style.height = gridSizeInput.value + "px";
+  clearGrid();
+  grid.style.width = gridSizeInput.value + "px";
   const gridSizeInPx = parseInt(gridSizeInput.value);
   const cellSizeInPx = 25;
   const cellsPerSide = gridSizeInPx / cellSizeInPx;
 
   for (let i = 0; i < cellsPerSide * cellsPerSide; i++) {
     const cell = document.createElement("div");
-    flexContainer.appendChild(cell);
+
+    cell.addEventListener("mousedown", () => {
+      isDrawing = true;
+    });
+    cell.addEventListener("click", () => {
+      cell.classList.add("colorized-cell");
+    });
+    cell.addEventListener("mouseup", () => {
+      isDrawing = false;
+    });
+    cell.addEventListener("mouseover", drawCell);
+
+    grid.appendChild(cell);
   }
 }
 
+function drawCell(event) {
+  if (isDrawing) {
+    event.target.classList.add("colorized-cell");
+  }
+}
+
+function clearGrid() {
+  grid.innerHTML = "";
+}
+
+let isDrawing = false;
+
 const gridSizeInput = document.querySelector(".grid-input");
-const flexContainer = document.querySelector(".flex-container");
+const grid = document.querySelector(".grid");
 const button = document.querySelector("button");
 
 gridSizeInput.addEventListener("input", fillGrid);
+button.addEventListener("click", fillGrid);
 
-button.addEventListener("click", () => {
-  flexContainer.innerHTML = "";
-  fillGrid();
-});
+fillGrid();
